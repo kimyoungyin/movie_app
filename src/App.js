21572eebbdba2241f9f905a1644a5f1ac1551720
kimyoungyin <mafa1234@naver.com>
2021-08-04 22:47:16 +0900

    basic react movie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import "./App.css";
+import "./style.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
-function App() {
-    return <div className="App">Hello</div>;
-}
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [movieList, setMovieList] = useState([]);
+
+    const getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+        setMovieList(movies);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+    return (
+        <div className="App">
+            {loading ? (
+                <div className="loader">
+                    <div className="loader-text">Loading..</div>
+                </div>
+            ) : (
+                <div className="movies">
+                    {movieList.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster={movie.medium_cover_image}
+                            genres={movie.genres}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
